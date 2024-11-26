43dbf64e3bc47e5292c49144d37ca8f81ceee365
Yean <kjan0929@naver.com>
2024-11-26 10:58:16 +0900

    style: 레이아웃 기본 세팅

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,5 +1,15 @@
 import { ReactNode } from "react";
+import styled from "styled-components";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return <Container>{children}</Container>;
 };
+
+const Container = styled.div`
+  min-width: 360px;
+  max-width: 400px;
+  height: 100vh;
+  margin: 0 auto;
+  background-color: white;
+  color: #1d1d1d;
+`;
