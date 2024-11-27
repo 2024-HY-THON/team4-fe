e215327a8c0569db6364a121432c225a898cbff7
Yean <kjan0929@naver.com>
2024-11-27 22:24:37 +0900

    fix: 빌드오류 수정

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
-import './profile.css'; // CSS 파일을 import
-import settingIcon from '@assets/profile/setting.svg'
+import { useState } from "react";
+import "./profile.css"; // CSS 파일을 import
+import settingIcon from "@assets/profile/setting.svg";
 
 export const ProfilePage = () => {
   const [userInfo] = useState({
-    name: '남윤서',
-    profileImage: '', // 유저 이미지
+    name: "남윤서",
+    profileImage: "", // 유저 이미지
   });
 
-  const [recipes, setRecipes] = useState(['산책하기', '음악듣기', '명상하기']);
+  // 빌드 오류 주석처리
+  // const [recipes, setRecipes] = useState(["산책하기", "음악듣기", "명상하기"]);
 
   return (
     <div className="profile-page">
@@ -28,11 +29,11 @@ export const ProfilePage = () => {
 
       <div className="recipebox">
         <h3>나만의 해방 레시피</h3>
-      <ul>
-        <li>산책하기 <span>></span></li>
-        <li>음악듣기 <span>></span></li>
-        <li>명상하기 <span>></span></li>
-      </ul>
+        <ul>
+          <li>산책하기</li>
+          <li>음악듣기</li>
+          <li>명상하기</li>
+        </ul>
         <button className="add-recipe">새로운 레시피 추가</button>
       </div>
     </div>
