4a2dc831cdc6ae065e7c6e3a5aba4955cfb25415
nam yoonseo <ysnam0123@naver.com>
2024-11-27 20:50:09 +0900

    style: 마이 페이지 스타일링

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,3 +1,40 @@
+import React, { useState } from 'react';
+import './profile.css'; // CSS 파일을 import
+import settingIcon from '@assets/profile/setting.svg'
+
 export const ProfilePage = () => {
-  return <div>프로필 페이지</div>;
+  const [userInfo] = useState({
+    name: '남윤서',
+    profileImage: '', // 유저 이미지
+  });
+
+  const [recipes, setRecipes] = useState(['산책하기', '음악듣기', '명상하기']);
+
+  return (
+    <div className="profile-page">
+      <header className="header">
+        <h2>프로필</h2>
+        <span>
+          <img src={settingIcon} alt="Settings Icon" />
+        </span>
+      </header>
+
+      <div className="profile-info">
+        <div className="profile-picture">
+          <img src={userInfo.profileImage} alt="Profile" />
+        </div>
+        <p>{userInfo.name}</p>
+      </div>
+
+      <div className="recipebox">
+        <h3>나만의 해방 레시피</h3>
+      <ul>
+        <li>산책하기 <span>></span></li>
+        <li>음악듣기 <span>></span></li>
+        <li>명상하기 <span>></span></li>
+      </ul>
+        <button className="add-recipe">새로운 레시피 추가</button>
+      </div>
+    </div>
+  );
 };
